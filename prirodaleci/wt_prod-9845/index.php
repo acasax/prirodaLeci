744e732ac85e77e420cb15cee2aec90089d3a1df
Aleksandar Djordjevic <acasax@gmail.com>
2021-05-18 12:08:48 +0200

    jebada

--- prirodaleci/wt_prod-9845/index.php
+++ prirodaleci/wt_prod-9845/index.php
@@ -2,7 +2,6 @@
 <html class="wide wow-animation" lang="en">
 
 <head>
-    <title>Priroda leči</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
