9bc9ae064e9bc0f9e24887951fb25890a31e1910
ksnenad998 <nenad.peric@outlook.com>
2021-05-31 18:11:09 +0200

    baza

--- prirodaleci.sql
+++ prirodaleci.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 18, 2021 at 06:22 PM
+-- Generation Time: May 31, 2021 at 04:28 PM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.10
 
@@ -20,6 +20,26 @@ SET time_zone = "+00:00";
 --
 -- Database: `prirodaleci`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admins`
+--
+
+CREATE TABLE `admins` (
+  `id` int(11) NOT NULL,
+  `username` varchar(15) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `admins`
+--
+
+INSERT INTO `admins` (`id`, `username`, `password`) VALUES
+(1, 'admin', 'admin'),
+(2, 'admin', 'admin');
 
 -- --------------------------------------------------------
 
@@ -42,24 +62,40 @@ CREATE TABLE `deliveries` (
 
 CREATE TABLE `item` (
   `id` int(11) NOT NULL,
-  `name` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL
+  `name` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `img` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `order`
+-- Table structure for table `orders`
 --
 
-CREATE TABLE `order` (
+CREATE TABLE `orders` (
   `id` int(11) NOT NULL,
   `name` varchar(15) COLLATE utf8mb4_unicode_ci NOT NULL,
   `lastname` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `address` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `zip` int(5) NOT NULL,
   `time` datetime NOT NULL,
   `phone` varchar(15) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(35) COLLATE utf8mb4_unicode_ci NOT NULL,
   `status` enum('Neposlato','Poslato','Stiglo') COLLATE utf8mb4_unicode_ci NOT NULL,
   `note` text COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`id`, `name`, `lastname`, `address`, `zip`, `time`, `phone`, `email`, `status`, `note`) VALUES
+(1, 'Nenad', 'Peric', 'Jovana Bjelica 1', 37000, '2021-05-24 15:21:33', '0600455722', 'nenad.peric@outlook.com', 'Neposlato', 'qweqwe'),
+(6, 'Boki', 'Nikolic', 'Kurolska', 37000, '2021-05-26 12:31:42', '0603539351', 'nikolicborisav@gmail.com', 'Neposlato', 'test'),
+(43, 'Perke Peric', 'qweqe', 'Jovana Bjelica 1', 37000, '2021-05-27 16:45:50', '1231231', 'kostamilovanovic@yahoo.com', 'Neposlato', 'qqqq'),
+(44, 'Perke Peric', 'qeqeqe', 'Jovana Bjelica 1', 37000, '2021-05-27 16:49:58', '1231231231', 'ksnesha98@gmail.com', 'Neposlato', 'eee'),
+(47, 'Perke Peric', 'qeqeqeqe', 'Jovana Bjelica 1', 37000, '2021-05-27 16:53:11', '1231231233', 'ksnesha98@gmail.com', 'Neposlato', 'qq'),
+(48, 'Perke Peric', 'Dominic', 'Jovana Bjelica 1', 37000, '2021-05-27 16:57:55', '123123123', 'ksnesha98@gmail.com', 'Neposlato', 'ee');
 
 -- --------------------------------------------------------
 
@@ -70,9 +106,17 @@ CREATE TABLE `order` (
 CREATE TABLE `order_items` (
   `id` int(11) NOT NULL,
   `fk_order` int(11) NOT NULL,
-  `fk_item` int(11) NOT NULL,
+  `item` varchar(11) COLLATE utf8mb4_unicode_ci NOT NULL,
   `quantity` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `order_items`
+--
+
+INSERT INTO `order_items` (`id`, `fk_order`, `item`, `quantity`) VALUES
+(6, 47, 'nonbak', 1),
+(7, 48, 'nonbak', 1);
 
 -- --------------------------------------------------------
 
@@ -82,13 +126,33 @@ CREATE TABLE `order_items` (
 
 CREATE TABLE `storage_items` (
   `id` int(11) NOT NULL,
-  `fk_item` int(11) NOT NULL,
+  `item` varchar(11) COLLATE utf8mb4_unicode_ci NOT NULL,
   `quantity` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
+-- Dumping data for table `storage_items`
+--
+
+INSERT INTO `storage_items` (`id`, `item`, `quantity`) VALUES
+(1, 'nonbak', 111),
+(2, 'postkovid', 1111),
+(3, 'nonbak', 55),
+(4, 'postkovid', 55),
+(5, 'nonbak', 1),
+(6, 'postkovid', 1),
+(7, 'nonbak', 1),
+(8, 'postkovid', 2);
+
+--
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `admins`
+--
+ALTER TABLE `admins`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `deliveries`
@@ -103,16 +167,17 @@ ALTER TABLE `item`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `order`
+-- Indexes for table `orders`
 --
-ALTER TABLE `order`
+ALTER TABLE `orders`
   ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `order_items`
 --
 ALTER TABLE `order_items`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_order` (`fk_order`);
 
 --
 -- Indexes for table `storage_items`
@@ -123,6 +188,12 @@ ALTER TABLE `storage_items`
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `admins`
+--
+ALTER TABLE `admins`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `deliveries`
@@ -137,22 +208,32 @@ ALTER TABLE `item`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `order`
+-- AUTO_INCREMENT for table `orders`
 --
-ALTER TABLE `order`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `orders`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
 
 --
 -- AUTO_INCREMENT for table `order_items`
 --
 ALTER TABLE `order_items`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `storage_items`
 --
 ALTER TABLE `storage_items`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `order_items`
+--
+ALTER TABLE `order_items`
+  ADD CONSTRAINT `fk_order` FOREIGN KEY (`fk_order`) REFERENCES `orders` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
