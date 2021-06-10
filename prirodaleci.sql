83532650176e67a6e9bba11c8170e560c54cc78b
ksnenad998 <nenad.peric@outlook.com>
2021-06-10 18:14:52 +0200

    baza

--- prirodaleci.sql
+++ prirodaleci.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 04, 2021 at 04:57 PM
+-- Generation Time: Jun 10, 2021 at 06:13 PM
 -- Server version: 10.4.18-MariaDB
 -- PHP Version: 8.0.3
 
